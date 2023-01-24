3a9217c4a7d03fffbce923f60cb92cfed63ce758
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-01-24 14:24:38 -0500

    Initial Commit

---
+++ Go Apollo/Go_ApolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  Go_ApolloApp.swift
+//  Go Apollo
+//
+//  Created by Parth Antala on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct Go_ApolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
